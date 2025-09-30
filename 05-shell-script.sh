e4557133fdf770315006eeac2e7e68ddf5ceb9fe
Loknath <devopsloknath94@gmail.com>
2025-09-30 21:27:18 +0530

    adding some conversation

--- 05-shell-script.sh
+++ 05-shell-script.sh
@@ -2,3 +2,6 @@
 echo "please enter your username"
 read -s username
 echo "username is $username"
+echo "please enter the password"
+read -s password
+echo "your username is $username, your password is $password" 
