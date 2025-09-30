b4e019182e809dce64c365189b3bc2beb1118c01
Loknath <devopsloknath94@gmail.com>
2025-09-30 20:34:51 +0530

    adding some conversation

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-PERSON1=ramesh
-PERSON2=suresh
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1 :: hello  $PERSON2 gud mrng"
 echo "$PERSON2 :: very gud mrng $PERSON1"
 echo "$PERSON1 :: what is the plan today $PERSON2" 
