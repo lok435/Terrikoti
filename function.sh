cf4fda3069f9a14f7e2251113ab034cfa93970ab
Loknath <devopsloknath94@gmail.com>
2025-10-03 22:24:10 +0530

    modfied

--- function.sh
+++ function.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-if [ $1 -ne - 0 ]
-then
+validate(){
+
+ if [ $1 -ne - 0 ]
+ then
     echo -e " $R ERROR:: $2 .. is FAILED $N "
     exit 1
-else 
+ else 
     echo -e "$G $2 .. success $N"
-fi
+ fi
+}
