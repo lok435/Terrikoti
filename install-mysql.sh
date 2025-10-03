ecce204f14538e4307a029a9c4b30caef2adcb5d
Loknath <devopsloknath94@gmail.com>
2025-10-03 23:46:19 +0530

    Hello

--- install-mysql.sh
+++ install-mysql.sh
@@ -12,14 +12,12 @@ then
  echo -e $R "ERROR:: please run this script with root access $N"
 else 
  echo "your root user"
-<<<<<<< HEAD
 fi
-yum install mysql -y
-=======
+yum install mysql -y 
 fi # closing tag for this block
 
 sudo yum install https://dev.mysql.com/get/mysql80-community-release-el8-1.noarch.rpm -y
 validate $? "installing mysql"
 fi
  
->>>>>>> eaf958d68f577fbaaadb9fae47c3ff2d5185d595
+
